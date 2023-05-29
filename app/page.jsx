a52fea1629f78a3336aa163913e1ae328779647e
Shafayathub <shafayat.ms@gamilcom>
2023-05-29 19:55:40 +0600

    components is under construction

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
 
 const home = () => {
-  return <div>home</div>;
+  return (
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">
+        Discover & share
+        <br className="min-md:hidden" />
+        <span className="orange_gradient text-center">AI-Powered Prompts</span>
+      </h1>
+      <p className="desc">
+        It is a web based tool to discover & share AI Prompts.
+      </p>
+      {/* feed */}
+    </section>
+  );
 };
 
 export default home;
